e2ae9a306ccbf8ef40f0db944e60991a4d146982
Rajthegreat123 <rajshekharan2020@gmail.com>
2022-08-22 19:55:47 +0000

    update

--- server.js
+++ server.js
@@ -138,4 +138,4 @@ io.on("connection", socket => {
     })
 })
 
-server.listen(5000, () => console.log("Server started on port 5000..."));
+server.listen(process.env.PORT || 5000, () => console.log("Server started on port 5000..."));
